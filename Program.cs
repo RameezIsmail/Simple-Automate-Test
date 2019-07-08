1d2b8c83f927628c7126dd7bef175b41c7a40f7a
RameezIsmail <52487361+RameezIsmail@users.noreply.github.com>
2019-07-08 18:21:13 +1200

    Edit test done

--- Program.cs
+++ Program.cs
@@ -89,7 +89,8 @@ namespace TimeMaterials_Auto
             Lastpage.Click();
 
             //To find if the test went successful or not
-            if (driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[7]/td[1]")).Text == "Rammy")
+            if (driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[3]/td[1]")).Text == "Rammy")
+
             {
                 Console.WriteLine("Create sucessfully, test passed.");
             }
@@ -101,8 +102,48 @@ namespace TimeMaterials_Auto
             //Pause the the browser so the aotmation can catch up
             Thread.Sleep(1000);
 
+            //Click edit button
+            IWebElement editbutton = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[3]/td[5]/a[1]"));
+            editbutton.Click();
+
+            //Clear previous code name and add new one
+            IWebElement codenamechange = driver.FindElement(By.Id("Code"));
+            codenamechange.Clear();
+            codenamechange.SendKeys("Amazing");
+            
+            //Clear previous description name and add new one
+            IWebElement Descnamechange = driver.FindElement(By.Id("Description"));
+            Descnamechange.Clear();
+            Descnamechange.SendKeys("Countdown");
+
+            /*IWebElement Pricechange = driver.FindElement(By.XPath("//*[@id='TimeMaterialEditForm']/div/div[4]/div/span[1]/span/input[1]"));
+            Pricechange.Clear();
+            Pricechange.SendKeys("200");
+            */
+
+            //Click save button
+            IWebElement Savebutton1 = driver.FindElement(By.XPath("//*[@id='SaveButton']"));
+            Savebutton1.Click();
+
+            Thread.Sleep(1000);
+
+            //Navigate to last page
+            IWebElement Lastpageagain = driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[4]/a[4]/span"));
+            Lastpageagain.Click();
+
+
+            if (driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[3]/td[1]")).Text == "Amazing")
+
+            {
+                Console.WriteLine("Edit sucessfully, test passed.");
+            }
+            else
+            {
+                Console.WriteLine("Edit test Failed");
+            }
+
             //Click Delete button
-            driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[7]/td[5]/a[2]")).Click();
+            //driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[7]/td[5]/a[2]")).Click();
 
 
         }
